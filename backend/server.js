48ae25f8fa75244d74078574706d006abc6e06d1
Teknath <jhateknath64@gmail.com>
2023-01-19 13:10:48 +0530

    Bug fixed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 const { connectDatabase } = require("./config/database");
 const cloudinary = require("cloudinary");
+const path = require("path");
 
 connectDatabase();
 
