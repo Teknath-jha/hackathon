aa413cd8f6b62c4cbe90be1bcc2c6784a8209f40
Teknath <jhateknath64@gmail.com>
2023-01-19 16:17:22 +0530

    Bug fixed

--- backend/app.js
+++ backend/app.js
@@ -23,16 +23,17 @@ app.use("/api/v1", user);
 
 // ---------deployment--------------------
 
-// __dirname = path.resolve();
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/build")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-//   });
-// } else {
+__dirname = path.resolve();
+
+// else {
 //   app.get("/", (req, res) => {
 //     res.send("API is running");
 //   });
 // }
+
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
+});
 
 module.exports = app;
